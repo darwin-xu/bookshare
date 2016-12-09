3a2c5257dafa77e49bee9f3151aa08f044a3cfec
Darwin Xu <darwin.xu@icloud.com>
2016-12-09 21:13:01 +0800

    DEV: Add a login page.

---
+++ ios/bookshare/MyViewController.swift
@@ -0,0 +1,122 @@
+//
+//  MyViewController.swift
+//  bookshare
+//
+//  Created by 许待文 on 2016/12/7.
+//  Copyright © 2016年 darwin. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 4
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "MyIdentifier", for: indexPath)
+
+        switch indexPath.row {
+        case 0:
+            NSLog("LoginIdentifier")
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LoginIdentifier", for: indexPath)
+            cell.selectionStyle = UITableViewCellSelectionStyle.none
+            return cell
+        default:
+            NSLog("ConfigIdentifier")
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ConfigIdentifier", for: indexPath)
+
+            switch indexPath.row {
+            case 1:
+                cell.imageView?.image = UIImage(named: "home")
+                cell.textLabel?.text = "我的书库"
+            case 2:
+                cell.textLabel?.text = "求书"
+            default:
+                cell.textLabel?.text = "愿望"
+            }
+            //c
+            return cell
+        }
+
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0:
+            return 140
+        default:
+            return 44
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
