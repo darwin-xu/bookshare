b53dd737eaa6d3c1ac8b1b175b65d9711bbb6bae
Darwin Xu <darwin.xu@icloud.com>
2017-01-01 16:01:24 +0800

    DEV: prepare to support generate DOC from code.

--- server/src/test/java/com/bookshare/BookshareApplicationTests.java
+++ server/src/test/java/com/bookshare/BookshareApplicationTests.java
@@ -1,16 +1,51 @@
 package com.bookshare;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration
+@WebAppConfiguration
 public class BookshareApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before(value = "")
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(MockMvcRestDocumentation.documentationConfiguration(this.restDocumentation)).build();
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        this.mockMvc.perform(get("/oook").accept(MediaTypes.HAL_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("_links.crud", is(notNullValue())));
+    }
 
 }
