1fb4b0c049bf920776b75eaa8d21b41571899e2d
darwin-xu <xu.dai.wen@126.com>
2017-07-02 11:24:37 +0800

    TEST: migrate to TestNG

--- server/src/test/java/com/bookshare/utility/StringUtilTest.java
+++ server/src/test/java/com/bookshare/utility/StringUtilTest.java
@@ -1,8 +1,7 @@
 package com.bookshare.utility;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import static org.testng.AssertJUnit.assertEquals;
+import org.testng.annotations.Test;
 
 public class StringUtilTest {
 
